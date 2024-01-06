c568841b14c37573d3404a3ec40be32ab04e5978
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-06 12:07:09 -0300

    feat: environment config

---
+++ src/env.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  MODE: z.enum(['production', 'local']),
+  VITE_APP_URL: z.string(),
+})
+
+export const env = envSchema.parse(import.meta.env)
