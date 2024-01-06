30fcec68117e72df1b3c76bd3624da0bc4c23067
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-06 12:08:17 -0300

    feat: create `NotFound` page and implement in to `routes`

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+export function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-4xl font-bold">A página não foi encontrada</h1>
+      <p className="text-accent-foreground text-2xl">
+        Voltar para tela {' '}
+        <Link className="text-orange-500 hover:underline" to="/">
+          Inicial
+        </Link>
+        .
+      </p>
+    </div>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { RouteObject, createBrowserRouter } from 'react-router-dom'
+import { NotFound } from './pages/404'
 import { AppLayout } from './pages/_layouts/app'
 
 const routes: RouteObject[] = [
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />
   },
 ]
 
