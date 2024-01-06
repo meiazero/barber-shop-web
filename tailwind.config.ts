49289ef5615e439cfadf9f32d641bd55ebced846
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-06 12:07:32 -0300

    style: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
+  darkMode: 'class',
   content: [
     './pages/**/*.{ts,tsx,js,jsx,mdx}',
     './components/**/*.{ts,tsx,js,jsx,mdx}',
