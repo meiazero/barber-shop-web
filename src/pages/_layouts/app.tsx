9f5170e252dd4d57b2b214b6dd0b01d6ff78e0b6
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-07 23:29:01 -0300

    Add header component and update layout

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,9 +1,13 @@
+import { Header } from "@/components/header"
 import { Outlet } from "react-router-dom"
 
 export function AppLayout() {
   return (
-    <main className="bg-background min-h-screen antialiased">
-      <Outlet />
+    <main className="bg-background flex min-h-screen flex-col antialiased">
+      <Header />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+        <Outlet />
+      </div>
     </main>
   )
 }
