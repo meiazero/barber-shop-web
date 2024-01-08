519cde7a62109f819a491c36b94ac8511f05a583
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-08 00:22:58 -0300

    Add useLocation hook to NavMenu component

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -1,23 +1,31 @@
 import { NavConfig } from "@/config/nav"
 import { NavItem } from "@/types"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 export function NavMenu() {
+  const { pathname } = useLocation()
+
   return (
     <nav>
       <ul className="flex flex-row gap-3">
         {NavConfig.map((link, index) => (
-          <NavMenuItem {...link} key={index} />
+          <NavMenuItem link={link} path={pathname} key={index} />
         ))}
       </ul>
     </nav>
   )
 }
 
-function NavMenuItem(link: NavItem) {
+function NavMenuItem({ link, path }: { link: NavItem; path?: string }) {
   return (
-    <li className="hover:underline">
-      <Link to={link.href} title={link.title}>
+    <li>
+      <Link
+        to={link.href}
+        title={link.title}
+        className={`py-1 hover:border-b hover:border-zinc-900 ${
+          path == link.href ? "border-b border-zinc-900" : ""
+        }`}
+      >
         {link.label}
       </Link>
     </li>
