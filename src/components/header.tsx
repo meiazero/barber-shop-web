6d9ea643f87dd2e07b591a48ab06ff03741256ca
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-07 23:27:42 -0300

    feat: `Header` component

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import { NavMenu } from "@/components/nav-menu"
+import { Link } from "react-router-dom"
+
+export function Header() {
+  return (
+    <header className="flex h-16 w-full items-center border-b border-zinc-200">
+      <div className="container flex flex-row items-center justify-between">
+        <h1 className="text-4xl font-bold">
+          <Link to="/" title="Home">
+            itaxi
+          </Link>
+        </h1>
+        <NavMenu />
+      </div>
+    </header>
+  )
+}
