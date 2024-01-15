0cc0384399022a9359f9a19723b62d8ce639584c
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-15 19:08:06 -0300

    Update 404 page heading alignment

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom"
 export function NotFound() {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-2">
-      <h1 className="text-4xl font-bold">A página não foi encontrada</h1>
+      <h1 className="text-center text-4xl font-bold">
+        A página não foi encontrada
+      </h1>
       <p className="text-2xl text-accent-foreground">
         Voltar para tela{" "}
         <Link className="text-orange-500 hover:underline" to="/">
