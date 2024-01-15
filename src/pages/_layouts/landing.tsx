bf240b0d1957c449573e05a67a2aa431b2cd31ee
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-15 18:51:32 -0300

    feat: landing page layout

---
+++ src/pages/_layouts/landing.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { Outlet } from "react-router-dom"
+
+export function LandingLayout() {
+  return (
+    <main className="flex min-h-screen flex-col bg-background antialiased">
+      <Header />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+        <Outlet />
+      </div>
+    </main>
+  )
+}
