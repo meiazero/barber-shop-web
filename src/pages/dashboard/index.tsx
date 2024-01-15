57ac92007d6ce75f7703ce160720710904b431d8
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-15 00:31:43 -0300

    Add Dashboard page

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet-async"
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+      </div>
+    </>
+  )
+}
