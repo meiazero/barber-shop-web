4acd73d5a0ee56d88cfa067e85143e37fe19ff92
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-15 18:52:15 -0300

    feat: `UserProvider` to App component

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,17 @@
 import { Helmet, HelmetProvider } from "react-helmet-async"
 import { RouterProvider } from "react-router-dom"
 import { Toaster } from "./components/ui/sonner"
+import { UserProvider } from "./providers/AuthProvider"
 import { router } from "./routes"
 
 export default function App() {
   return (
-    <HelmetProvider>
-      <Helmet titleTemplate="%s | barber.shop" />
-      <RouterProvider router={router} />
-      <Toaster richColors />
-    </HelmetProvider>
+    <UserProvider>
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | barber.shop" />
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </HelmetProvider>
+    </UserProvider>
   )
 }
