7d7f45578ce3fbfadb10ed58992eac90ef7d0e31
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-16 11:51:06 -0300

    feat: test connection with extern api

--- src/pages/app/home/index.tsx
+++ src/pages/app/home/index.tsx
@@ -1,6 +1,37 @@
+import { env } from "@/env"
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { Helmet } from "react-helmet-async"
 
 export function Home() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    let isCancelled = false
+
+    async function fetchAndSetData() {
+      const options = {
+        method: "GET",
+        url: env.VITE_API_URL,
+      }
+      try {
+        const { data } = await axios.request(options)
+
+        if (!isCancelled) {
+          setData(data)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchAndSetData()
+
+    return () => {
+      isCancelled = true
+    }
+  }, [])
+
   return (
     <>
       <Helmet title="página inicial" />
@@ -10,6 +41,11 @@ export function Home() {
         <p className="text-xl">
           Agende seu horário com os melhores barbeiros da região.
         </p>
+        {data.length > 0 && (
+          <pre className="font-mono text-sm">
+            {JSON.stringify(data, null, 2)}
+          </pre>
+        )}
       </div>
     </>
   )
