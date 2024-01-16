79886a03dd08e0c7f7cbcf90258eb32895c5e6bc
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-16 12:44:34 -0300

    Add current date and schedulers table to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,23 @@
+import { getCurrentDate } from "@/lib/utils"
 import { Helmet } from "react-helmet-async"
+import { SchedulersTable } from "./schedulers-table"
 
 export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
-      <div className="flex flex-col gap-4">
-        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
-      </div>
+      <main className="container flex flex-col gap-y-4">
+        <div className="flex w-full flex-row items-center justify-between">
+          <h1 className="w-fit rounded-md border bg-slate-100 px-2 py-1 text-2xl font-bold uppercase">
+            Meiazero Barbearia
+          </h1>
+          <p className="text-md font-bold text-black-smoke">
+            {getCurrentDate()}
+          </p>
+        </div>
+
+        <SchedulersTable />
+      </main>
     </>
   )
 }
