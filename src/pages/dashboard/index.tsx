bce19ef1ec58758661b55e2f787327d7e3f75082
Emanuel Avila <avilaemanueel@gmail.com>
2024-01-16 13:11:59 -0300

    feat: ui more responsible

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,9 +6,9 @@ export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
-      <main className="container flex flex-col gap-y-4">
+      <main className="flex flex-col gap-y-4 md:container">
         <div className="flex w-full flex-row items-center justify-between">
-          <h1 className="w-fit rounded-md border bg-slate-100 px-2 py-1 text-2xl font-bold uppercase">
+          <h1 className="text-md w-fit rounded-md border bg-slate-100 px-2 py-1 font-bold uppercase md:text-2xl">
             Meiazero Barbearia
           </h1>
           <p className="text-md font-bold text-black-smoke">
